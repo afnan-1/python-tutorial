1b168a5fa7beca7ff650aa9eeaad23a85df8c670
afnan-1 <roman.nadeem@gmail.com>
2021-02-07 00:08:41 +0500

    tuts2

---
+++ tuts2.py
@@ -0,0 +1,18 @@
+x = '8'
+afnan = "afnan123"
+afnan123_ = "adsad"
+
+a = 1 #int
+x = 4 #int
+b = '123' #str
+c = 123
+
+# floogle = 'mark'
+# name = 'mark'
+# print(floogle)
+# print(name)
+# print(a+x) # 1+4 
+# print(a) #1+123
+
+my_name = 'afnan'
+print(my_name)
